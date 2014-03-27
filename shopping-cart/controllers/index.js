f36f8ea98ed4fec433511cc55dbf59fbec75d72d
Matt Edelman <medelman@paypal.com>
2014-03-27 16:18:58 -0700

    adding shopping cart wip files

---
+++ shopping-cart/controllers/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+var Product = require('../models/productModel');
+
+
+module.exports = function (app) {
+
+
+	app.get('/', function (req, res) {
+
+		Product.find(function (err, prods) {
+			if (err) {
+				console.log(err);
+			}
+			var model = {
+				products: prods
+			}
+			res.render('index', model);
+		})
+
+	});
+};
---
+++ shopping-cart/test/index.js
@@ -0,0 +1,43 @@
+/*global describe:false, it:false, before:false, after:false, afterEach:false*/
+
+'use strict';
+
+
+var app = require('../index'),
+    kraken = require('kraken.next'),
+    express = require('express'),
+    request = require('supertest');
+
+
+describe('/', function () {
+
+    var app, mock;
+
+
+    beforeEach(function (done) {
+        app = express();
+        app.on('start', done);
+        app.use(kraken());
+
+        mock = app.listen(1337);
+
+    });
+
+
+    afterEach(function (done) {
+        mock.close(done);
+    });
+
+
+    it('should say "hello"', function (done) {
+        request(mock)
+            .get('')
+            .expect(200)
+            .expect('Content-Type', /html/)
+            .expect(/Hello, /)
+            .end(function(err, res){
+                done(err);
+            });
+    });
+
+});
